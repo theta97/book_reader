f1418de8972a4c60a4cc0665a606e9b2267d3282
Haifa <mhaifa027@gmail.com>
2023-05-13 13:32:09 +0500

    Testing Database Connection

--- ebook_reader/lib/screens/homenew.dart
+++ ebook_reader/lib/screens/homenew.dart
@@ -1,13 +1,30 @@
 import 'package:ebook_reader/genre.dart';
 import 'package:flutter/cupertino.dart';
-// import 'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:ebook_reader/book.dart';
 import 'package:ebook_reader/newbooks.dart';
 import 'package:ebook_reader/bookcards.dart';
 
 // Future<http.Response> fetchAlbum() {
 //   return http.get(Uri.parse('https://gorest.co.in/public/v1/users'));
 // }
+
+class Book {
+  String Book_ID;
+  String bookname;
+  String author_id;
+  String bookpic;
+  String bookpdf;
+
+  Book({required this.Book_ID, required this.bookname, required this.author_id, required this.bookpic, required this.bookpdf});
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+        Book_ID: json['Book_ID'], bookname: json['bookname'], author_id: json['bookpic'], bookpic: json['author_id'], bookpdf: json['bookpdf']);
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key});
@@ -17,6 +34,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // final String apiUrl = "https://book_reader.test/";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,30 +128,81 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           GenreCard(),
-          Expanded(
-            child: ListView(
-                // shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
-                      // height: 10,
-                      width: 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/1.jpg"),
-                          fit: BoxFit.fill,
+          // Expanded(
+          //   child: ListView(
+          //       // shrinkWrap: true,
+          //       scrollDirection: Axis.horizontal,
+          //       children: [
+          //         Padding(
+          //           padding: const EdgeInsets.only(left: 10),
+          //           child: Container(
+          //             // height: 10,
+          //             width: 150,
+          //             decoration: BoxDecoration(
+          //               borderRadius: BorderRadius.circular(10),
+          //               image: DecorationImage(
+          //                 image: AssetImage("assets/images/1.jpg"),
+          //                 fit: BoxFit.fill,
+          //               ),
+          //             ),
+          //           ),
+          //         ),
+          //       ]),
+          // )
+        Expanded(child: FutureBuilder<List<Book>>(
+            future: fetchBooks(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Book> books = snapshot.data as List<Book>;
+                return ListView.builder(
+                    itemCount: books.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(8),
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Text(books[index].bookname),
+                            Text(books[index].author_id),
+                            Text(books[index].bookpic),
+                            Text(books[index].bookpdf),
+                            InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const PdfSc(
+                                        pdflink:
+                                            "https://localhost/ebook_reader/pdfs/test.pdf"),
+                                  ),
+                                );
+                              },
+                            )
+                          ],
                         ),
-                      ),
-                    ),
-                  ),
-                ]),
-          )
+                      );
+                    });
+              }
+              if (snapshot.hasError) {
+                print(snapshot.error.toString());
+                return Text('error');
+              }
+              return CircularProgressIndicator();
+            }, 
+        
+        ),
+        ),
         ],
       ),
     );
+  }
+
+  Future<List<Book>> fetchBooks() async {
+    var response = await http.get(Uri.parse("http://book_reader.test/php/"));
+    return (json.decode(response.body)['data'] as List)
+        .map((e) => Book.fromJson(e))
+        .toList();
   }
 }
 
