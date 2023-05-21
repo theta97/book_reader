863c7b443d3c93b671be9157e5c54affcc05aabe
Haifa <mhaifa027@gmail.com>
2023-05-21 16:58:56 +0500

    Fetch Genres

--- ebook_reader/lib/genre.dart
+++ ebook_reader/lib/genre.dart
@@ -1,4 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<Genre>> fetchGenre() async {
+  var url = Uri.parse('https://10.0.2.2:7128/api/Genres');
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => Genre.fromJson(data)).toList();
+  } else {
+    throw Exception('Unexpected error occured!');
+  }
+}
+
+class Genre {
+  final int genreId;
+  final String genre1;
+
+  Genre({required this.genreId, required this.genre1});
+
+  factory Genre.fromJson(Map<String, dynamic> json) {
+    return Genre(
+      genreId: json['genreId'],
+      genre1: json['genre1'],
+    );
+  }
+}
 
 class GenreCard extends StatelessWidget {
   const GenreCard({super.key});
@@ -7,148 +34,145 @@ class GenreCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                height: 40,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0B4066),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Genre1',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                height: 40,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0B4066),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Genre2',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                height: 40,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0B4066),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Genre3',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            InkWell(
-              onTap: () {},
-              child: Container(
-                alignment: Alignment.center,
-                height: 40,
-                width: 140,
-                decoration: BoxDecoration(
-                  color: Color(0xFF0B4066),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Text(
-                  'Genre4',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            // SizedBox(
-            //   width: 10,
-            // ),
-            // InkWell(
-            //   onTap: () {},
-            //   child: Container(
-            //     alignment: Alignment.center,
-            //     height: 40,
-            //     width: 140,
-            //     decoration: BoxDecoration(
-            //       color: Color(0xFF0E402D),
-            //       borderRadius: BorderRadius.circular(10),
-            //     ),
-            //     child: Text(
-            //       'Genre5',
-            //       style: TextStyle(
-            //         fontSize: 16,
-            //         fontWeight: FontWeight.w700,
-            //         color: Colors.white,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // SizedBox(
-            //   width: 10,
-            // ),
-            // InkWell(
-            //   onTap: () {},
-            //   child: Container(
-            //     alignment: Alignment.center,
-            //     height: 40,
-            //     width: 140,
-            //     decoration: BoxDecoration(
-            //       color: Color(0xFF0E402D),
-            //       borderRadius: BorderRadius.circular(10),
-            //     ),
-            //     child: Text(
-            //       'Genre6',
-            //       style: TextStyle(
-            //         fontSize: 16,
-            //         fontWeight: FontWeight.w700,
-            //         color: Colors.white,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-          ],
-        ),
-      ),
+      child: FutureBuilder<List<Genre>>(
+          future: fetchGenre(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 40,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0B4066),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        snapshot.data![index].genre1,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  );
+                  SizedBox(
+                    width: 10,
+                  );
+                },
+              );
+            }
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            // return CircularProgressIndicator();
+            return const Text("It work??");
+          }),
     );
   }
 }
+
+
+ // child: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      //   child: Row(
+      //     children: [
+      //       InkWell(
+      //         onTap: () {},
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           height: 40,
+      //           width: 140,
+      //           decoration: BoxDecoration(
+      //             color: Color(0xFF0B4066),
+      //             borderRadius: BorderRadius.circular(10),
+      //           ),
+      //           child: const Text(
+      //             'Genre1',
+      //             style: TextStyle(
+      //               fontSize: 16,
+      //               fontWeight: FontWeight.w700,
+      //               color: Colors.white,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       SizedBox(
+      //         width: 10,
+      //       ),
+      //       InkWell(
+      //         onTap: () {},
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           height: 40,
+      //           width: 140,
+      //           decoration: BoxDecoration(
+      //             color: Color(0xFF0B4066),
+      //             borderRadius: BorderRadius.circular(10),
+      //           ),
+      //           child: const Text(
+      //             'Genre2',
+      //             style: TextStyle(
+      //               fontSize: 16,
+      //               fontWeight: FontWeight.w700,
+      //               color: Colors.white,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       const SizedBox(
+      //         width: 10,
+      //       ),
+      //       InkWell(
+      //         onTap: () {},
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           height: 40,
+      //           width: 140,
+      //           decoration: BoxDecoration(
+      //             color: Color(0xFF0B4066),
+      //             borderRadius: BorderRadius.circular(10),
+      //           ),
+      //           child: const Text(
+      //             'Genre3',
+      //             style: TextStyle(
+      //               fontSize: 16,
+      //               fontWeight: FontWeight.w700,
+      //               color: Colors.white,
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       const SizedBox(
+      //         width: 10,
+      //       ),
+      //       InkWell(
+      //         onTap: () {},
+      //         child: Container(
+      //           alignment: Alignment.center,
+      //           height: 40,
+      //           width: 140,
+      //           decoration: BoxDecoration(
+      //             color: Color(0xFF0B4066),
+      //             borderRadius: BorderRadius.circular(10),
+      //           ),
+      //           child: const Text(
+      //             'Genre4',
+      //             style: TextStyle(
+      //               fontSize: 16,
+      //               fontWeight: FontWeight.w700,
+      //               color: Colors.white,
+      //             ),
+      //           ),
+      //         ),
+      //       ),         
+      //     ],
+      //   ),
+      // ),
