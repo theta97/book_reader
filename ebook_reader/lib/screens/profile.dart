0c0ca80e29252d2bfb709ff20920fa3ec8a443f3
Haifa <haistudent070@gmail.com>
2023-06-08 01:21:26 -0700

    Sign Up

--- ebook_reader/lib/screens/profile.dart
+++ ebook_reader/lib/screens/profile.dart
@@ -1,22 +1,97 @@
+import 'package:ebook_reader/models/reader_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key});
+
+   ProfilePage({super.key, });
+
+   
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: Text('This is the temp appbar title of profile page'),
+        title: Text("Provider Demo"),
+      ),
+      body: Center(
+        child: GestureDetector(
+
+          child: Center(
+            child: Container(
+              height: 70,
+              padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 20),
+              margin: const EdgeInsets.only(left: 40, right: 40),
+              child: const Center(
+                child: Text("Home page", style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF74beef),
+                ),),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(4, 4),
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-4, -4),
+                      blurRadius: 15,
+                      spreadRadius: 1,
+                    ),
+
+                  ]
+              ),
+            ),
+          ),
+        ),
       ),
     );
-    throw UnimplementedError();
+   
+  // return Scaffold(
+  //     appBar: AppBar(actions: [
+  //       TextButton(
+  //           onPressed: () async {
+  //             final SharedPreferences? prefs = await _prefs;
+  //             prefs?.clear();
+  //             Get.offAll(AuthScreen());
+  //           },
+  //           child: Text(
+  //             'logout',
+  //             style: TextStyle(color: Colors.white),
+  //           ))
+  //     ]),
+  //     body: Center(
+  //       child: Column(
+  //         children: [
+  //           Text('Welcome home'),
+  //           TextButton(
+  //               onPressed: () async {
+  //                 final SharedPreferences? prefs = await _prefs;
+  //                 print(prefs?.get('token'));
+  //               },
+  //               child: Text('print token'))
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  
   }
 }
